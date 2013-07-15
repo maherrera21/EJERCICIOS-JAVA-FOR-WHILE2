da814bd7e9bea282f056b42151ece414b67772fa
miguelithoh <maherrera21@utpl.edu.ec>
2013-07-14 23:49:03 -0500

    Create Interes.java

---
+++ Interes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.text.DecimalFormat;
+
+
+public class Interes {   
+    public static void main(String args[]){
+		DecimalFormat dosDigitos = new DecimalFormat( "0.00" );
+		int anios=1;
+		double monto=0;
+		for(int a=0;a<anios;a++)
+		{
+			for(int b=0;b<12;b++)
+			{
+				monto*=1.03;
+				monto+=1000;
+			}
+		}
+		System.out.println("en "+anios+" se tendra ahorrado $ "+dosDigitos.format(monto));
+	}
+}
+    
+
