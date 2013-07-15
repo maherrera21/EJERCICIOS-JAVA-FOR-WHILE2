78a0fb3bcd13cbb8ec34f712ae502dba9f720dbf
miguelithoh <maherrera21@utpl.edu.ec>
2013-07-14 23:48:30 -0500

    Create Nuempar.java

---
+++ Nuempar.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Scanner;
+
+public class Nuempar {
+    public static void main(String[] args)  {
+    {
+           int dato=0, sum=0;
+            Scanner leer=new Scanner(System.in);
+                    System.out.println("\ningresar un numero");
+                    dato=leer.nextInt();
+            
+            if((dato>=2)&&(dato%2==0)){
+                    for(int i=2;i<=dato;i++){ 
+               sum= sum+i;}
+               System.out.println("\n La suma es: "+sum); 
+            }else{
+                    System.out.println("ERROR");               
+      }
+     }
+   }
+}
+
+    
+    
+
